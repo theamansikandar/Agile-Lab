e70322e159cd36d48603bb2f589f15d4c09c57f9
Aman Sikandar <amansikandar558@gmail.com>
2026-07-23 10:46:24 +0530

    calc

---
+++ calculator.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+if __name__ == "__main__":
+    x = 10
+    y = 5
+    print("Sum:", add(x, y))
+    print("Difference:", subtract(x, y))
