f1c7f14aef9058e39e96883613b22cfec19a46e6
Aman <amansikandar558@gmail.com>
2026-07-30 11:05:48 +0530

    updatd to division

--- calc.py
+++ calc.py
@@ -2,11 +2,14 @@ def add(a, b):
     return a + b
 def subtract(a, b):
     return a - b
-def multiply(a,b):
+def product(a,b):
     return a*b
+def division(a,b):
+    return a/b
 if __name__ == "__main__":
     x = 10
     y = 5
     print("Sum:", add(x, y))
     print("Difference:", subtract(x, y))
-    print("Product: ",multiply(x,y))
+    print("Product: ",product(x,y))
+    print("Division: ",division(x,y))
